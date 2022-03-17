b1a993d41c9058206e928f5150817c4ce7643596
Aya <ayagafar88@gmail.com>
2022-03-17 11:42:52 +0200

    editing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { render } from 'creditcardpayments/creditCardPayments';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'payment-with-Stripe';
+  constructor() {
+    render({
+      id: '#payments',
+      currency: 'USD',
+      value: '100.00',
+      onApprove: (details) => {
+        alert('transaction successfull');
+      },
+    });
+  }
 }
